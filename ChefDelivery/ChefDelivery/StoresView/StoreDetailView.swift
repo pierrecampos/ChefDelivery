a80ccc797d51015a00833067d52ce0415b660897
Pierre Dias <cdias.pierre@gmail.com>
2024-02-20 08:11:42 -0300

    [feat] Construção tela StoreDetail - #2

--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -12,43 +12,69 @@ struct StoreDetailView: View {
     var store: StoreType
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Image(store.headerImage)
-                .resizable()
-                .scaledToFit()
-            
-            HStack {
-                Text(store.name)
-                    .font(.title)
-                    .bold()
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Image(store.headerImage)
+                    .resizable()
+                    .scaledToFit()
                 
-                Spacer()
-                
-                Image(store.logoImage)
-            }
-            .padding(.vertical, 8)
-            .padding()
-            
-            HStack {
-                Text(store.location)
-                
-                Spacer()
-                
-                ForEach(1...store.stars, id: \.self) { _ in
-                    Image(systemName: "star.fill")
-                        .foregroundStyle(.yellow)
-                        .font(.caption)
+                HStack {
+                    Text(store.name)
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Image(store.logoImage)
                 }
-            }
-            .padding(.vertical, 8)
-            .padding()
-            
-            Text("Produtos")
-                .font(.title2)
-                .bold()
+                .padding(.vertical, 8)
                 .padding()
+                
+                HStack {
+                    Text(store.location)
+                    
+                    Spacer()
+                    
+                    ForEach(1...store.stars, id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .font(.caption)
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding()
+                
+                Text("Produtos")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                
+                ForEach(store.products) { product in
+                    HStack(spacing: 8) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(product.name)
+                                .bold()
+                            
+                            Text(product.description)
+                                .foregroundStyle(.black.opacity(0.5))
+                            
+                            Text("\(product.price)")
+                        }
+                        
+                        Spacer()
+                        
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .frame(width: 120, height: 120)
+                            .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(store.name)
         }
-        .navigationTitle(store.name)
     }
 }
 
