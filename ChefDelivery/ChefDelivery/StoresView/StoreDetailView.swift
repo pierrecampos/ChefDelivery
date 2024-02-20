8a55ac6b2e55786d66d8eaf81ae95150f38bf648
Pierre Dias <cdias.pierre@gmail.com>
2024-02-20 07:53:54 -0300

    [feat] Construção tela StoreDetail - #1

--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -12,7 +12,43 @@ struct StoreDetailView: View {
     var store: StoreType
     
     var body: some View {
-        Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                Text(store.name)
+                    .font(.title)
+                    .bold()
+                
+                Spacer()
+                
+                Image(store.logoImage)
+            }
+            .padding(.vertical, 8)
+            .padding()
+            
+            HStack {
+                Text(store.location)
+                
+                Spacer()
+                
+                ForEach(1...store.stars, id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(.yellow)
+                        .font(.caption)
+                }
+            }
+            .padding(.vertical, 8)
+            .padding()
+            
+            Text("Produtos")
+                .font(.title2)
+                .bold()
+                .padding()
+        }
+        .navigationTitle(store.name)
     }
 }
 
