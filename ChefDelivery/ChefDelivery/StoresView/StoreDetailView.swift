e1c25608f613b772a91729a2c2396a4a095bb6c5
Pierre Dias <cdias.pierre@gmail.com>
2024-02-21 08:36:16 -0300

    [feat] Tela ProductDetailView implementada

--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -51,25 +51,31 @@ struct StoreDetailView: View {
                     .padding()
                 
                 ForEach(store.products) { product in
-                    HStack(spacing: 8) {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text(product.name)
-                                .bold()
+                    NavigationLink {
+                        ProductDetailView(product: product)
+                    } label: {
+                        HStack(spacing: 8) {
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text(product.name)
+                                    .bold()
+                                
+                                Text(product.description)
+                                    .foregroundStyle(.black.opacity(0.5))
+                                    .multilineTextAlignment(.leading)
+                                
+                                Text(product.price.formatToCurrency())
+                            }
                             
-                            Text(product.description)
-                                .foregroundStyle(.black.opacity(0.5))
+                            Spacer()
                             
-                            Text(product.price.formatToCurrency())
+                            Image(product.image)
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                                .frame(width: 120, height: 120)
+                                .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
                         }
-                        
-                        Spacer()
-                        
-                        Image(product.image)
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                            .frame(width: 120, height: 120)
-                            .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
+                        .foregroundStyle(.black)
                     }
                 }
                 .padding()
