cdac4ef12e9c95304e99042c98f814682831e3e8
Pierre Dias <cdias.pierre@gmail.com>
2024-03-01 23:45:32 -0300

    [feat] adicionado home-logo + DragGesture

--- ChefDelivery/ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/ChefDelivery/HomeView/HomeView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var isAnimating = false
+    @State private var imageOffset: CGSize = .zero
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                
                 Circle()
                     .foregroundStyle(.colorRed)
                     .frame(width: isAnimating ? 200 : 0)
@@ -47,14 +47,27 @@ struct HomeView: View {
                         .opacity(isAnimating ? 1: 0)
                         .offset(y: isAnimating ? 0 : -40)
                     
-                    Spacer()
-                    
+                    Image("home-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(radius: 60)
+                        .padding(isAnimating ? 32 : 92)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width,  y: imageOffset.height)
+                        .gesture(DragGesture().onChanged({ gesture in
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                imageOffset = gesture.translation
+                            }
+                        }).onEnded({ _ in
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                imageOffset = .zero
+                            }
+                        }))
                 }.onAppear {
                     withAnimation(.easeInOut(duration: 1)) {
                         isAnimating = true
                     }
-                    
-                }
+                }.preferredColorScheme(.light)
             }
         }
     }
