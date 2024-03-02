5d98532cc170323896c08bc8479b9688d396c161
Pierre Dias <cdias.pierre@gmail.com>
2024-03-01 23:03:26 -0300

    [feat] Adição de círculos animados HomeView

--- ChefDelivery/ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/ChefDelivery/HomeView/HomeView.swift
@@ -12,29 +12,50 @@ struct HomeView: View {
     @State private var isAnimating = false
     
     var body: some View {
-        VStack {
-            Text("Chef Delivery")
-                .font(.system(size: 48))
-                .fontWeight(.heavy)
-                .foregroundStyle(.colorRed)
-                .opacity(isAnimating ? 1: 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Text("Peça as suas comidas favoritas no conforto da sua casa.")
-                .font(.title2)
-                .padding()
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.black.opacity(0.7))
-                .opacity(isAnimating ? 1: 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Spacer()
-            
-        }.onAppear {
-            withAnimation(.easeInOut(duration: 1)) {
-                isAnimating = true
+        GeometryReader { geometry in
+            ZStack {
+                
+                Circle()
+                    .foregroundStyle(.colorRed)
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(x: isAnimating ? 50 : -50 , y: isAnimating ? 100 : -100)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 1)
+                
+                Circle()
+                    .foregroundStyle(.colorRedDark)
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(
+                        x: isAnimating ?  geometry.size.width - 50 : geometry.size.width  + 50,
+                        y: isAnimating ? geometry.size.height - 50 : geometry.size.height + 50)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 1)
+                
+                VStack {
+                    Text("Chef Delivery")
+                        .font(.system(size: 48))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.colorRed)
+                        .opacity(isAnimating ? 1: 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Text("Peça as suas comidas favoritas no conforto da sua casa.")
+                        .font(.title2)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.black.opacity(0.7))
+                        .opacity(isAnimating ? 1: 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Spacer()
+                    
+                }.onAppear {
+                    withAnimation(.easeInOut(duration: 1)) {
+                        isAnimating = true
+                    }
+                    
+                }
             }
-            
         }
     }
 }
