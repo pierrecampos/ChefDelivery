93188999dc065c4ac2e2ad898c857e4eac3917d9
Pierre Dias <cdias.pierre@gmail.com>
2024-04-04 19:06:32 -0300

    [feat] Adicionado animação para Carrousel

--- ChefDelivery/ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/ChefDelivery/CarouselView/CarouselTabView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    
+    // MARK: - Attributes
+    @State private var currentId = bannerMocks[0].id
+    
     var body: some View {
-        TabView {
+        TabView(selection: $currentId) {
             ForEach(bannerMocks) { mock in
                 CarouselItemView(order: mock)
+                    .tag(mock.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page)
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 1)) {
+                    var currentIndex = bannerMocks.firstIndex { $0.id == currentId } ?? 0
+                    currentIndex = currentIndex >= bannerMocks.count - 1 ? 0 : currentIndex + 1
+                    currentId = bannerMocks[currentIndex].id
+                }                
+            }
+        }
     }
 }
 
